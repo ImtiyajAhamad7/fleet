5761a9cb44b90191a5c027a0441f367fb67210a0
imtiyaj ahamad <imtiyaj.ahamad@tekinspirations.com>
2025-09-03 16:45:48 +0530

    frotnend search page

---
+++ frontend/src/app/searchAndBookVehicle/page.tsx
@@ -0,0 +1,9 @@
+import SearchAndBookVehicle from "./searchAndBookVehicle";
+
+export default function Page() {
+  return (
+    <div className="container mx-auto py-8">
+      <SearchAndBookVehicle />
+    </div>
+  );
+}
